a63557b6497ab07ba4694ce387790ba4294074d1
Dan Smith <Dan@Dans-MacBook-Pro.local>
2017-01-17 10:57:35 -0500

    This is my first upload to github using the command line on OSX

---
+++ Workout.py
@@ -0,0 +1,22 @@
+import pickle #converts python code to binary code 
+
+
+
+try:
+    day = []
+    bpart = []
+    with open('workoutinfo.txt') as workout_document:
+        for each_line in workout_document:
+            (day_,body_part) = each_line.split(':')
+            day.append(day_)
+            bpart.append(body_part)                   
+except IOError as error_:
+    print('This is the error:', error_ )
+    pass
+
+while True:
+    day_of_week = input("Please enter the day of the week ")
+    if day_of_week in day:
+            print ("Today is", len(bpart))
+    else:
+        print("That isnt a day of the week!")
